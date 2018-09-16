236d2c8ead678eaff9cfddcafc918bac0aa4418c
Alexandre Marcondes <alex@marcondesian.net>
2018-09-16 12:27:08 -0700

    Add dispute_coordinator group

--- constants.rb
+++ constants.rb
@@ -1,1 +1,7 @@
-DISPUTE_TOOLS_GROUP = { :name => 'dispute_pro', :full_name => 'Dispute Pro'}
+DISPUTE_TOOLS_GROUP = { :name => 'dispute_pro', :full_name => 'Dispute Pro' }
+
+# For creating private message threads in discourse
+# for the dispute coordinator admins (probably only
+# a single admin) that will manage the dispute
+# private message thread in discourse.
+DISPUTE_COORDINATOR_GROUP = { :name => 'dispute_coordinator', :full_name => 'Dispute Coordinator' }
